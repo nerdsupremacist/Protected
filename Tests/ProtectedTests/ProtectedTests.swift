b76e96f096edd6496b8819c85d358ee6f0ad5365
Mathias Quintero <me@quintero.io>
2022-06-05 00:39:50 +0100

    Adding a simple test

---
+++ Tests/ProtectedTests/ProtectedTests.swift
@@ -0,0 +1,49 @@
+import XCTest
+@testable import Protected
+
+final class ProtectedTests: XCTestCase {
+    func testSampleRights() throws {
+        let book = createBook()
+        XCTAssertEqual(book.title, "Don Quixote")
+        XCTAssertEqual(book.author, "Miguel de Cervantes")
+        XCTAssertEqual(book.isbn?.first, "0")
+
+        book.title = "La cueva de salamanca"
+        XCTAssertEqual(book.title, "La cueva de salamanca")
+    }
+}
+
+func createBook() -> Protected<Book, PrePublishRights> {
+    let book = Book()
+    book.title = "Don Quixote"
+    book.author = "Miguel de Cervantes"
+    book.isbn = "0060188707"
+    return Protected(value: book, by: .prePublish)
+}
+
+class Book {
+  var title: String?
+  var author: String?
+  var isbn: String?
+}
+
+extension RightsManifest where Self == PrePublishRights {
+    static var prePublish: PrePublishRights {
+        return PrePublishRights()
+    }
+}
+
+struct PrePublishRights: RightsManifest {
+    typealias ProtectedType = Book
+
+    let title = Write(\.title)
+    let author = Read(\.author)
+    let isbn = Read(\.isbn, protectedBy: FirstCharacterOnly())
+}
+
+struct FirstCharacterOnly: RightsManifest {
+    typealias ProtectedType = String
+
+    let first = Read(\.first)
+}
+
