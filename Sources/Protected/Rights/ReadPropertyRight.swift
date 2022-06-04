431888ce85d66d1a98a5b0e969865c26b1a58b15
Mathias Quintero <me@quintero.io>
2022-06-05 00:39:23 +0100

    adding multiple combinations of right protection possibilities

--- Sources/Protected/Rights/ReadPropertyRight.swift
+++ Sources/Protected/Rights/ReadPropertyRight.swift
@@ -10,5 +10,45 @@ public struct ReadPropertyRight<ProtectedType, Value, Strategy: RightResolutionS
         self.strategy = strategy
     }
 
+    public init<Rights : RightsManifest>(_ keyPath: KeyPath<ProtectedType, Value>,
+                                         protectedBy rights: Rights) where Rights.ProtectedType == Value, Strategy == ProtectedRightResolutionStrategy<Rights> {
+
+        self.init(keyPath, strategy: ProtectedRightResolutionStrategy(rights: rights))
+    }
+
+    public init<Rights : RightsManifest>(_ keyPath: KeyPath<ProtectedType, Value>,
+                                         protectedBy rights: Rights) where Optional<Rights.ProtectedType> == Value, Strategy == OptionalRightResolutionStrategy<ProtectedRightResolutionStrategy<Rights>> {
+
+        self.init(keyPath, strategy: OptionalRightResolutionStrategy(strategy: ProtectedRightResolutionStrategy(rights: rights)))
+    }
+
+    public init<Rights : RightsManifest>(_ keyPath: KeyPath<ProtectedType, Value>,
+                                         protectedBy rights: Rights) where Array<Rights.ProtectedType> == Value, Strategy == ArrayRightResolutionStrategy<ProtectedRightResolutionStrategy<Rights>> {
+
+        self.init(keyPath, strategy: ArrayRightResolutionStrategy(strategy: ProtectedRightResolutionStrategy(rights: rights)))
+    }
+
+    public init<Rights : RightsManifest>(_ keyPath: KeyPath<ProtectedType, Value>,
+                                         protectedBy rights: Rights) where Optional<Array<Rights.ProtectedType>> == Value, Strategy == OptionalRightResolutionStrategy<ArrayRightResolutionStrategy<ProtectedRightResolutionStrategy<Rights>>> {
+
+        self.init(keyPath, strategy: OptionalRightResolutionStrategy(strategy: ArrayRightResolutionStrategy(strategy: ProtectedRightResolutionStrategy(rights: rights))))
+    }
+
+    public init<Rights : RightsManifest>(_ keyPath: KeyPath<ProtectedType, Value>,
+                                         protectedBy rights: Rights) where Array<Optional<Rights.ProtectedType>> == Value, Strategy == ArrayRightResolutionStrategy<OptionalRightResolutionStrategy<ProtectedRightResolutionStrategy<Rights>>> {
+
+        self.init(keyPath, strategy: ArrayRightResolutionStrategy(strategy: OptionalRightResolutionStrategy(strategy: ProtectedRightResolutionStrategy(rights: rights))))
+    }
+
+    public init<Rights : RightsManifest>(_ keyPath: KeyPath<ProtectedType, Value>,
+                                         protectedBy rights: Rights) where Optional<Array<Optional<Rights.ProtectedType>>> == Value, Strategy == OptionalRightResolutionStrategy<ArrayRightResolutionStrategy<OptionalRightResolutionStrategy<ProtectedRightResolutionStrategy<Rights>>>> {
+
+        self.init(keyPath, strategy: OptionalRightResolutionStrategy(strategy: ArrayRightResolutionStrategy(strategy: OptionalRightResolutionStrategy(strategy: ProtectedRightResolutionStrategy(rights: rights)))))
+    }
 }
 
+extension ReadPropertyRight where Strategy == SimpleRightResolutionStrategy<Value> {
+    public init(_ keyPath: KeyPath<ProtectedType, Value>) {
+        self.init(keyPath, strategy: SimpleRightResolutionStrategy())
+    }
+}
