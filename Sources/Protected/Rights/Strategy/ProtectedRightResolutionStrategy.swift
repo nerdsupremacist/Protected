04be081a3eb4f643d7bd8aa5940c1cfdd5daf4bf
Mathias Quintero <me@quintero.io>
2022-06-05 00:39:07 +0100

    Adding basic reslution strategy to wrap again in a protected

---
+++ Sources/Protected/Rights/Strategy/ProtectedRightResolutionStrategy.swift
@@ -0,0 +1,17 @@
+
+import Foundation
+
+public struct ProtectedRightResolutionStrategy<Rights: RightsManifest>: RightResolutionStrategy {
+    public typealias Value = Rights.ProtectedType
+    public typealias Resolved = Protected<Value, Rights>
+
+    private let rights: Rights
+
+    init(rights: Rights) {
+        self.rights = rights
+    }
+
+    public func resolve(value: Rights.ProtectedType) -> Protected<Value, Rights> {
+        return Protected(value: value, by: rights)
+    }
+}
