56e2a2433e334e10bb74b0688f45d1f06660a47c
Mathias Quintero <me@quintero.io>
2022-06-05 00:38:45 +0100

    adding resolution strategy Adding basic protected wraper

---
+++ Sources/Protected/Protected.swift
@@ -0,0 +1,31 @@
+
+import Foundation
+
+@dynamicMemberLookup
+public class Protected<Value, Rights : RightsManifest> where Rights.ProtectedType == Value {
+    private var value: Value
+    private let rights: Rights
+
+    public init(value: Value, by rights: Rights) {
+        self.value = value
+        self.rights = rights
+    }
+
+    public subscript<T, Strategy : RightResolutionStrategy>(dynamicMember keyPath: KeyPath<Rights, ReadPropertyRight<Value, T, Strategy>>) -> Strategy.Resolved {
+        let right = rights[keyPath: keyPath]
+        return right.strategy.resolve(value: value[keyPath: right.keyPath])
+    }
+
+    public subscript<T>(dynamicMember keyPath: KeyPath<Rights, WritePropertyRight<Value, T>>) -> T {
+        get {
+            return value[keyPath: rights[keyPath: keyPath].keyPath]
+        }
+        set {
+            value[keyPath: rights[keyPath: keyPath].keyPath] = newValue
+        }
+    }
+
+    public func unsafeChangeRights<TransformedRights : RightsManifest>(to rights: TransformedRights) -> Protected<Value, TransformedRights> {
+        return Protected<Value, TransformedRights>(value: value, by: rights)
+    }
+}
